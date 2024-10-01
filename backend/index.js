129e7412ca4fd347cdff8d3a868e14d55b494c02
Pintu <pintu@Pintus-Laptop.lan>
2024-10-01 20:48:08 +0530

    change structure

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,14 @@ var cors = require('cors')
 const port=80;
 const app = express();
 
-app.use(cors())
+app.use(cors(
+    {
+        origin:"https://pintu-inotebook-backend.vercel.app/",
+        methods:["GET","POST","PUT","DELETE"],
+        credentials:true
+
+    }
+))
 
 app.use(express.json())
 
