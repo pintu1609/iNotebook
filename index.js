bb655648f43745a3a1aeee1a3285eddb1fd37db0
Pintu <pintu@Pintus-Laptop.local>
2024-10-02 20:39:32 +0530

    remove versel

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const  connectToMongo= require('../db')
+const  connectToMongo= require('./db')
 const express=require('express');
 var cors = require('cors')
 
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(cors(
     {
-        origin:"https://pintu-inotebook.vercel.app",
+        origin:"http://localhost:3000",
         methods:["GET","POST","PUT","DELETE"],
         credentials:true
 
@@ -18,9 +18,13 @@ app.use(cors(
 
 app.use(express.json())
 
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
 //available routes
-app.use('/api/auth',require('../routes/auth'))
-app.use('/api/notes',require('../routes/notes'))
+app.use('/api/auth',require('./routes/auth'))
+app.use('/api/notes',require('./routes/notes'))
 
 
 
